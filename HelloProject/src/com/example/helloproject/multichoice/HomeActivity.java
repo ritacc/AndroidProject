5ae2df2b57836b2660d2ba1c39fea4e894f7abb5
ritacc <ritacc@126.com>
2014-02-24 17:49:09 +0800

    Signed-off-by: ritacc <ritacc@126.com>

---
+++ HelloProject/src/com/example/helloproject/multichoice/HomeActivity.java
@@ -0,0 +1,26 @@
+package com.example.helloproject.multichoice;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.actionbarsherlock.app.SherlockListActivity;
+
+public class HomeActivity extends SherlockListActivity{
+//		@Override
+//	    protected void onCreate(Bundle savedInstanceState) {
+//	        super.onCreate(savedInstanceState);
+//	        setContentView(R.layout.activity_main);
+//	        String[] titles = getActivityTitles();
+//	        setListAdapter(new ArrayAdapter<String>(
+//	                this, android.R.layout.simple_list_item_1, android.R.id.text1, titles));
+//	    }
+//	    
+//	    @Override
+//	    protected void onListItemClick(ListView l, View v, int position, long id) {
+//	        Class<? extends Activity> class_ = activitiesInfo.get(position).activityClass;
+//	        Intent intent = new Intent(this, class_);
+//	        startActivity(intent);
+//	    }
+}
